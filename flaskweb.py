cb1c0a34efc92ef7ff53985cd2aa1dba1f6870c8
wolftongren <14285614@qq.com>
2017-07-10 23:43:26 +0800

    fixed a bug in flaskweb, if there is no data, set to [0,0,0]

--- flaskweb.py
+++ flaskweb.py
@@ -13,9 +13,12 @@ c = conn.cursor()
 def hello():
     d = datetime.datetime.now().date()
     c.execute('select shangzhang, xiadie, pingpan from mon where date = %s order by time desc limit 1', d)
-    v = c.fetchone();
-    conn.commit();
-    ones = [v[0], v[1], v[2]];
+    v = c.fetchone()
+    conn.commit()
+    if (v != None):
+        ones = [v[0], v[1], v[2]]
+    else:
+        ones = [0,0,0]
     print ones
     return render_template('mon.html', data=json.dumps(ones))
 
@@ -25,10 +28,14 @@ def getnew():
     d = datetime.datetime.now().date()
     c.execute('select shangzhang, xiadie, pingpan from mon where date = %s order by time desc limit 1', d)
     v = c.fetchone()
-    conn.commit();
-    top = [v[0], v[1], v[2]]
+    conn.commit()
+    if (v!=None):
+        top = [v[0], v[1], v[2]]
+    else:
+        top = [0,0,0]
     print top
     return json.dumps(top)
+
 
 
 app.run(host='0.0.0.0', port=8888, debug=True)
